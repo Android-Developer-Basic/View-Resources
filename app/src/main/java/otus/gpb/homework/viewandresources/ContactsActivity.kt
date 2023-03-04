cd68de45d5bfee1095a9bb2675681c60479b4736
Andrey Kharkovsky <andreykharkovsky@MacBook-Pro-de-Andrey.local>
2023-03-04 19:30:10 +0300

    resources

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -1,11 +1,18 @@
 package otus.gpb.homework.viewandresources
 
+import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.graphics.drawable.IconCompat
 
 class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
+        findViewById<Toolbar>(R.id.toolBar).setNavigationOnClickListener {
+            finish()
+        }
     }
 }
