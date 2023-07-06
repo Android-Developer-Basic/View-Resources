c47fd9a3cf8db477aa7e890dddd433b307dadac6
Ed_Krupenin <ed.krupenin@gmail.com>
2023-07-06 23:25:43 +0300

    home work

--- app/src/main/java/otus/gpb/homework/viewandresources/CartActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/CartActivity.kt
@@ -2,6 +2,7 @@ package otus.gpb.homework.viewandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -1,11 +1,71 @@
 package otus.gpb.homework.viewandresources
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
+import android.telephony.PhoneNumberUtils
+import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import java.util.Locale
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current date as the default date in the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(requireContext(), this, year, month, day)
+
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+
+        // Do something with the date chosen by the user
+    }
+}
 
 class ContactsActivity : AppCompatActivity() {
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+        val text: EditText = findViewById(R.id.phoneFieldEditText);
+
+        val textView: EditText  = findViewById(R.id.BirthdayFieldEditText)
+        val cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            textView.setText(sdf.format(cal.time))
+        }
+
+        textView.setOnClickListener {
+            DatePickerDialog(this@ContactsActivity, dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
     }
 }
