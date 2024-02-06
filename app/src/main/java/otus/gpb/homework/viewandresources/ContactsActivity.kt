0ef0e6a92df510450baeafac2c28df76dee2a889
glasolin <glasolin@gmail.com>
2024-02-06 21:52:26 +0300

    Action bar customization

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -1,24 +1,37 @@
 package otus.gpb.homework.viewandresources
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 class ContactsActivity : ActivityHelper() {
+    private val tag = "ContactsActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
 
         // showing the back button in action bar
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val actionBar = supportActionBar
+        requireNotNull(actionBar==null)
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+        //actionBar.setDisplayUseLogoEnabled(true)
+        //actionBar.setDisplayShowHomeEnabled(true)
     }
 
-    override fun onContextItemSelected(item: MenuItem): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.contacts, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // methods to control the operations that will
+    // happen when user clicks on the action buttons
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
+            R.id.search -> Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show()
+            android.R.id.home -> finish()
+            else -> Toast.makeText(this, "Else Clicked", Toast.LENGTH_SHORT).show()
         }
-        return super.onContextItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
 }
