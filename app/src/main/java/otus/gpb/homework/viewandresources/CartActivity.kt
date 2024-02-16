a4bdd872e1638b20db2dd24f903aef326b91ac92
glasolin <glasolin@gmail.com>
2024-02-16 16:03:22 +0300

    Cart activity, except list

--- app/src/main/java/otus/gpb/homework/viewandresources/CartActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/CartActivity.kt
@@ -1,10 +1,20 @@
 package otus.gpb.homework.viewandresources
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
+import java.net.URI
+import kotlin.math.floor
+
+data class CartItem (
+    val name:String,
+    val caption: String,
+    val price: Double,
+    val icon: String
+)
+
 
 class CartActivity : ActivityHelper() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +28,13 @@ class CartActivity : ActivityHelper() {
         actionBar!!.setDisplayHomeAsUpEnabled(true)
         actionBar.setDisplayUseLogoEnabled(true)
         actionBar.setDisplayShowHomeEnabled(true)
+
+        findViewById<TextView>(R.id.cart_count).text = resources.getQuantityString(R.plurals.cart_items_count,getNumberOfItems(),getNumberOfItems())
+        findViewById<TextView>(R.id.cart_order_total_sum).text= String.format("%.2f",getTotal())
+        findViewById<TextView>(R.id.cart_order_subtotal_sum).text= String.format("%.2f",getSubtotal())
+        findViewById<TextView>(R.id.cart_order_tax_sum).text= String.format("%.2f",getTax())
+        findViewById<TextView>(R.id.cart_order_shipping_sum).text= String.format("%.2f",getDeliveryPrice())
+
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.cart, menu)
@@ -34,4 +51,16 @@ class CartActivity : ActivityHelper() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun getNumberOfItems()=4
+
+    private fun getSubtotal()=36.0
+
+    private fun getTaxRate()=10.3
+
+    private fun getDeliveryPrice()=2.5
+
+    private fun getTax()=(getSubtotal()*getTaxRate())/100
+
+    private fun getTotal()=getSubtotal()+getTax()+getDeliveryPrice()
 }
