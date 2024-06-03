b933e8f2c8aec615d0209aa24f6525b317175e26
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-06-03 15:39:18 +0300

    Поулчился автокомплит а не дропдаунменю

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -1,10 +1,12 @@
 package otus.gpb.homework.viewandresources
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+
+
 
 class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,21 @@ class ContactsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_contacts)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+
+
+
+
+//        val COUNTRIES = arrayOf(
+//            "Belgium", "France", "Italy", "Germany", "Spain"
+//        )
+//        val adapter = ArrayAdapter(
+//            this,
+//            android.R.layout.simple_dropdown_item_1line, COUNTRIES
+//        )
+//        val textView = findViewById<AutoCompleteTextView>(R.id.autocomplete1)
+//        textView.setAdapter(adapter)
+
 
     }
 
