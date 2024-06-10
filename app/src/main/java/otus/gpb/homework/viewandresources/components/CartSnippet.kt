d98d49d294ff997ce9f73615b47a11b2a8cc7af2
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-06-10 20:21:55 +0300

    Ломается вертка не пойму почему

--- app/src/main/java/otus/gpb/homework/viewandresources/components/CartSnippet.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/components/CartSnippet.kt
@@ -2,7 +2,9 @@ package otus.gpb.homework.viewandresources.components
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.LinearLayout
+import otus.gpb.homework.viewandresources.databinding.CartSnippetBinding
 
 class CartSnippet @JvmOverloads constructor(
     context: Context,
@@ -10,6 +12,11 @@ class CartSnippet @JvmOverloads constructor(
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0,
 ) : LinearLayout (context, attrs, defStyleAttr, defStyleRes) {
+    private val binding : CartSnippetBinding
+    init {
+        binding = CartSnippetBinding.inflate(LayoutInflater.from(context), this)
+    }
+
 
 
 }
