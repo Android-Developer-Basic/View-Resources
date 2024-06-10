dec474f64b3faae8062fe782840c0f86615ba214
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-06-10 18:05:30 +0300

    Пробую вставить компонент

---
+++ app/src/main/java/otus/gpb/homework/viewandresources/components/CartSnippet.kt
@@ -0,0 +1,15 @@
+package otus.gpb.homework.viewandresources.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class CartSnippet @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : LinearLayout (context, attrs, defStyleAttr, defStyleRes) {
+
+
+}
